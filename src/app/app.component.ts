bf0578f9eddf08d7088ade10cff398ddc09a32f8
Saivenkat Pentakota <saivenkatpentakota@gmail.com>
2022-11-29 08:45:57 +0530

    Added media screen in app.component.css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import{faFacebook,faLinkedinIn} from '@fortawesome/free-brands-svg-icons';
+import{faFacebook,faLinkedinIn,faGoogle} from '@fortawesome/free-brands-svg-icons';
 import{faEnvelope} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -11,5 +11,6 @@ export class AppComponent {
   title = 'creativeapp';
   faFacebook = faFacebook;
   faEnvelope = faEnvelope;
-  faLinkedinIn = faLinkedinIn
+  faLinkedinIn = faLinkedinIn;
+  faGoogle = faGoogle;
 }
